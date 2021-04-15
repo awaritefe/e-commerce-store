74d8a0b6cef8e52d12987fb7301c6dfaf1eda318
DanielAwaritefe <awaritefe@gmail.com>
2021-04-15 20:35:12 +0100

    login navbar designed

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :product, only: [:index, :show]
+  resources :category, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
