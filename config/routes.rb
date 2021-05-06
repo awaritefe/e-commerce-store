1b2db4cb79ad581ed70cb90e5b1a930f8eb8bacd
DanielAwaritefe <awaritefe@gmail.com>
2021-05-06 20:00:28 +0100

    completed basic setup

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :product, only: [:index, :show]
-  resources :category, only: [:show]
+  resources :products, only: [:index, :show]
+  resources :categories, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
