83851d94caaf7a28eaa5f94c1f28b0d473d69202
DanielAwaritefe <awaritefe@gmail.com>
2021-05-07 16:22:12 +0100

    added css grid to list of product cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,12 @@
 puts "Cleaning database..."
 Product.destroy_all
 puts "Creating products..."
-jacket = { name: "Jacket", description: "light jacket", price: 89.99 }
+jacket = { name: "Vans Classic", description: "light classic jacket", price: 89.99 }
+skater_jacket = { name: "Vans Skater Jacket", description: "Skater jacket", price: 49.99 }
+shirt_jacket = { name: "Vans Shirt Jacket", description: "Shirt jacket", price: 69.99 }
 
 puts "PRODUCTS"
-[ jacket ].each do |attributes|
+[ jacket, skater_jacket, shirt_jacket ].each do |attributes|
   product = Product.create!(attributes)
   puts "Created #{product.name}"
 end
