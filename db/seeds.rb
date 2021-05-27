d3b574d0272b49ce1baae6b37f1d81bfd63d784a
DanielAwaritefe <awaritefe@gmail.com>
2021-05-27 22:50:23 +0100

    styled auth pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,33 @@
 puts "Cleaning database..."
 Product.destroy_all
 puts "Creating products..."
-jacket = { name: "Vans Classic", description: "light classic jacket", price: 89.99 }
-skater_jacket = { name: "Vans Skater Jacket", description: "Skater jacket", price: 49.99 }
-shirt_jacket = { name: "Vans Shirt Jacket", description: "Shirt jacket", price: 69.99 }
+jacket = {
+  name: "Vans Classic",
+  description: "Classic Jacket Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu erat non dui pulvinar semper eu ut est. Nullam laoreet tellus vestibulum facilisis fermentum. Nam nec ipsum et ipsum consectetur commodo sit amet sit amet lectus. Proin finibus vel orci ut vulputate. Cras porta, justo nec tincidunt dapibus, mi dolor feugiat nulla, ac tristique tellus mi non mauris.",
+  price: 89.99
+}
+skater_jacket = {
+  name: "Vans Skater Jacket",
+  description: "Skater jacket Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu erat non dui pulvinar semper eu ut est. Nullam laoreet tellus vestibulum facilisis fermentum. Nam nec ipsum et ipsum consectetur commodo sit amet sit amet lectus. Proin finibus vel orci ut vulputate. Cras porta, justo nec tincidunt dapibus, mi dolor feugiat nulla, ac tristique tellus mi non mauris.",
+  price: 49.99
+}
+shirt_jacket = {
+  name: "Vans Shirt Jacket",
+  description: "Shirt Jacket Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu erat non dui pulvinar semper eu ut est. Nullam laoreet tellus vestibulum facilisis fermentum. Nam nec ipsum et ipsum consectetur commodo sit amet sit amet lectus. Proin finibus vel orci ut vulputate. Cras porta, justo nec tincidunt dapibus, mi dolor feugiat nulla, ac tristique tellus mi non mauris.",
+  price: 69.99
+}
+
+puts "Creating categories..."
+mens = { name: "Mens"}
+womens = { name: "Womens"}
+accessories = { name: "Accessories" }
+sale = { name: "Sale" }
+
+puts "CATEGORIES"
+[ mens, womens, accessories, sale ].each do |attributes|
+  category = Category.create!(attributes)
+  puts "Created #{category.name}"
+end
 
 puts "PRODUCTS"
 [ jacket, skater_jacket, shirt_jacket ].each do |attributes|
