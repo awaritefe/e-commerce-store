a47b3ba736e2316a4045e1b2a2d08991a8ae799b
DanielAwaritefe <awaritefe@gmail.com>
2021-05-28 21:39:29 +0100

    added admin devise models and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :admins
   devise_for :users
   root to: 'pages#home'
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+  namespace :admin do
+    root to: 'products#index'
+    resources :products
+    resources :categories
+  end
+
 end
