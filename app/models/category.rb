0453d471f6e59578cf4f649561905b207c0d70e2
DanielAwaritefe <awaritefe@gmail.com>
2021-06-21 12:54:11 +0100

    Fixed naming and association error in category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ApplicationRecord
-  has_many :product_catergories
-  has_many :products, through: :product_catergories
+  has_many :product_categories
+  has_many :products, through: :product_categories
 end
